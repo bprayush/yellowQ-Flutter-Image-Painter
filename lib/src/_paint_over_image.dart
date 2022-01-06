03dd128bcd40ea1fd38400a7a58cb0c06b2001ed
BugTheDebugger <bprayush@yahoo.com>
2022-01-06 15:55:02 +0545

    removed clipper to ensure full page zoom

--- lib/src/_paint_over_image.dart
+++ lib/src/_paint_over_image.dart
@@ -483,39 +483,37 @@ class ImagePainterState extends State<ImagePainter> {
           Expanded(
             child: FittedBox(
               alignment: FractionalOffset.center,
-              child: ClipRect(
-                child: ValueListenableBuilder<Controller>(
-                  valueListenable: _controller,
-                  builder: (_, controller, __) {
-                    return ImagePainterTransformer(
-                      maxScale: 2.4,
-                      minScale: 1,
-                      panEnabled: controller.mode == PaintMode.none,
-                      scaleEnabled: widget.isScalable!,
-                      onInteractionUpdate: (details) =>
-                          _scaleUpdateGesture(details, controller),
-                      onInteractionEnd: (details) =>
-                          _scaleEndGesture(details, controller),
-                      child: CustomPaint(
-                        size: Size(_image!.width.toDouble(),
-                            _image!.height.toDouble()),
-                        willChange: true,
-                        isComplex: true,
-                        painter: DrawImage(
-                          image: _image,
-                          points: _points,
-                          paintHistory: _paintHistory,
-                          isDragging: _inDrag,
-                          update: UpdatePoints(
-                              start: _start,
-                              end: _end,
-                              painter: _painter,
-                              mode: controller.mode),
-                        ),
+              child: ValueListenableBuilder<Controller>(
+                valueListenable: _controller,
+                builder: (_, controller, __) {
+                  return ImagePainterTransformer(
+                    maxScale: 2.4,
+                    minScale: 1,
+                    panEnabled: controller.mode == PaintMode.none,
+                    scaleEnabled: widget.isScalable!,
+                    onInteractionUpdate: (details) =>
+                        _scaleUpdateGesture(details, controller),
+                    onInteractionEnd: (details) =>
+                        _scaleEndGesture(details, controller),
+                    child: CustomPaint(
+                      size: Size(
+                          _image!.width.toDouble(), _image!.height.toDouble()),
+                      willChange: true,
+                      isComplex: true,
+                      painter: DrawImage(
+                        image: _image,
+                        points: _points,
+                        paintHistory: _paintHistory,
+                        isDragging: _inDrag,
+                        update: UpdatePoints(
+                            start: _start,
+                            end: _end,
+                            painter: _painter,
+                            mode: controller.mode),
                       ),
-                    );
-                  },
-                ),
+                    ),
+                  );
+                },
               ),
             ),
           ),
